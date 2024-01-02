02ddb0a6b5410829384f06eb309149e348baca99
danielcamones <139814220+danielcamones@users.noreply.github.com>
2024-01-02 13:00:58 -0500

    Add files via upload

---
+++ cobrar/assets/Keys.js
@@ -0,0 +1,4 @@
+export default {
+  public: "pk_test_51OQf5ZAkp5jGNNt3jQyI2Fh3mtFq69OHyvUw4yLJtKVJEC8e1vuG9gS1p9XlWwIyzLxKL3WqLzHWZMD4mJ0Qu9tC004IzTE7Qn",
+  secret: "sk_test_51OQf5ZAkp5jGNNt39zZga1Zu5VOpy73OBUCEegkbV7fg15yz8kBGYNwdwspCghT2Na5cNaWVl8tiI2iTcJPjXw3t00jtwo7fga",
+}
